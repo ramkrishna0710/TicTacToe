af565967834eb3ec1439f232b4831b7003876c64
ramkrishna0710 <113228237+ramkrishna0710@users.noreply.github.com>
2024-04-28 15:03:12 +0530

    Adding files of tictactoe

---
+++ app/src/main/java/in/ram/tictactoe/AddPlayers.java
@@ -0,0 +1,50 @@
+package in.ram.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddPlayers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_players);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText playerOne = findViewById(R.id.playerOne);
+        EditText playerTwo = findViewById(R.id.playerTwo);
+        Button startGameButton = findViewById(R.id.startGameButton);
+
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getPlayerOneName = playerOne.getText().toString();
+                String getPlayerTwoName = playerTwo.getText().toString();
+
+                if (getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()) {
+                    Toast.makeText(AddPlayers.this, "Please enter the player name", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne",getPlayerOneName);
+                    intent.putExtra("playerTwo",getPlayerTwoName);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
